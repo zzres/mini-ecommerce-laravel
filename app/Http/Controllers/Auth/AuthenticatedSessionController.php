77250ed396fc7ce2b3e2bd1f79e3e078a3c6fb35
zzres <alvainz23@gmail.com>
2026-09-13 02:21:34 +0000

    Mise à jour, Rediriger l'admin vers son dashboard aprèsconnexion

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended(route('admin.dashbord', absolute: false));
         }
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('home', absolute: false));
     }
 
     /**
