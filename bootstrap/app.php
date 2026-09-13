353d022275f830529c633b36bbd7638c63b235f0
zzres <alvainz23@gmail.com>
2026-09-13 01:44:20 +0000

    Fix: faire confiance au proxy Render pour la détection HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
